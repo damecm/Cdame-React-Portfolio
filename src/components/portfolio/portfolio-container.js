b0a140cd7b89790033808d6565df3fae1b906b23
Cullen Dame <damecullen24@gmail.com>
2022-11-21 18:12:59 -0600

    props

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,14 +3,23 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-  // State
-  // Lifecycle hooks
+  constructor() {
+    super();
+
+    console.log("Portfolio container has rendered");
+  }
+  portfolioItems() {
+    const data = ["Quip", "Eventbrite", "Ministry Safe"];
+
+    return data.map(item => {
+      return <PortfolioItem title={item} url={"google.com"}/>;
+    });
+  }
   render() {
     return (
       <div>
         <h2>Portfolio items go here updated...</h2>
-
-        <PortfolioItem />
+        {this.portfolioItems()}
       </div>
     );
   }
